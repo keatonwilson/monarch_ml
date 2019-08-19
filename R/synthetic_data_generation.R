edb16839d4505b850944384bf4973fc6e0faa473
keatonwilson <keatonwilson@me.com>
2019-08-19 09:43:50 -0700

    Lots of changes to get it ready to run on a larger scale.

--- R/synthetic_data_generation.R
+++ R/synthetic_data_generation.R
@@ -17,7 +17,7 @@ monarch_data = monarch_data %>%
 
 #Also going to pull out some variables based on preliminary analysis
 #We want to keep 8, 14, 19, 5, 37, 2, 10, 5, 12, 15
-
+#
 
 monarch_small = monarch_data %>%
   dplyr::select(hectares, day_first_sighting, obs_37_norm, active_months_obs_norm,
@@ -26,12 +26,13 @@ monarch_small = monarch_data %>%
                 contains("_5"), contains("_37"), contains("_2"), 
                 contains("_10"), contains("_12"), contains("_15"))
 
-write_csv(monarch_small, "./data/monarch_data_real.csv")
+write_csv(monarch_small, "./data/monarch_data_small.csv")
 
+#Smoothing parameters
 smooth_list = as.list(rep("density", 45))
 names(smooth_list) = names(monarch_data)
 
-syn_monarch = synthpop::syn(monarch_small, k = 500000, method = "norm")
+syn_monarch = synthpop::syn(monarch_small, k = 50000, method = "norm")
 
 # compare(syn_monarch, monarch_data)
 
